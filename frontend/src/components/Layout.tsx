3aea2820411fe17e6a87577b5ed008f6992d7557
Dan Andre AreviloV <46893770+dinover@users.noreply.github.com>
2025-08-07 12:41:52 -0300

    cleanup: remove debug console.log from Layout component

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -35,10 +35,6 @@ const Layout: React.FC = () => {
 
   const allNavigation = [...navigation, ...adminNavigation];
 
-  // Debug: Log user role
-  console.log('User role:', user?.role);
-  console.log('Admin navigation items:', adminNavigation.length);
-
   const handleLogout = () => {
     logout();
     navigate('/login');
